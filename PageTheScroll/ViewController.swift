86a2f57deeec855156d211b392dd1037f8f9492b
kost9klinov <kost9klinov>
2018-06-28 19:07:31 +0300

    need add a custom gesture recognizer

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -10,12 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     var images = [UIImageView]()
-    
-    
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var contentWidth: CGFloat = 0.0
+        
+        for x in 0...2 {
+            
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+          
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            contentWidth += newX
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+
+        }
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
+        
     }
 
    
